d412daf53c3aa12222d5c869d51844e88697bfc2
Mila Data <miladata.sys@gmail.com>
2023-02-17 12:16:57 -0300

    First steps
    Configuring the compiler
    Debugging TypeScript

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let message = "Hello World!";
+message = "Hello World!";
+console.log("mensaje!");
+let message2 = "Bye!";
+message = "Bye World!";
+console.log("Bye!");
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+let message: string = "Hello World!"
+
+message= "Hello World!"
+
+console.log("mensaje!")
+
+let message2: string = "Bye!"
+
+message = "Bye World!"
+
+console.log("Bye!")
+
